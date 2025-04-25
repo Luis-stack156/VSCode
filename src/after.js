95615284f76cc43cca7874798e4cf2e065a5ac03
Luis <castilloaponteluisalexander@gmail.com>
2025-04-25 10:10:42 -0400

    primer proyecto

---
+++ src/after.js
@@ -0,0 +1,14 @@
+const fn = () => {
+    const holamundo = "buenos dias"
+let b = "buenas tardes"
+console.log(b);
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "buenas noches"
+
+    console.log(a);
+    
+}
